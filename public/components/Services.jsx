643ca1302f148649ba2bad2dcb8e8c65a3b76487
khushi303 <maankhushi303@gmail.com>
2024-03-18 16:58:28 +0530

    adding spacing

--- public/components/Services.jsx
+++ public/components/Services.jsx
@@ -3,7 +3,7 @@ import ServiceCard from './ServiceCard';
 export default function Services(props) {
     return (
         <>
-            <div className="container mx-auto max-w-[1164px] px-3">
+            <div className="container mx-auto max-w-[1164px] px-3 pt-[60px]">
                 <h2 className="ff_mazzardregular lg:text-[52px] sm:text-[32px] text-[20px] font-semibold text-white leading-[110%] text-center mb-3">{props.ServiveHeading}</h2>
                 <p className='lg:text-[16px] sm:text-[14px] text-[12px] font-normal leading-[150%] text-white opacity-80 text-center mt-[12px] mb-[60px]'>{props.Para1} <span className='md:block'>online assets are safe and your reputation intact.</span></p>
                 <div className='bg-gradient-to-br from-[#00BCD4] to-[#3145EC] max-w-[784px] h-[2px] w-full mb-8 relative mx-auto'>
