e65b6235d996843923ce18d253f27ef7bcebbbc0
khushi303 <maankhushi303@gmail.com>
2024-03-21 16:08:04 +0530

    change in ellipse

--- public/components/Footer.jsx
+++ public/components/Footer.jsx
@@ -59,9 +59,9 @@ export default function Footer() {
                     </ul>
                 </div>
             </div>
-            <div className='w-[206px] h-[206px] bg-[#8000FF] blur-[100px] absolute z-[1] bottom-[20%] right-[30%] animate-pulse'></div>
-            <div className='w-[206px] h-[206px] bg-[#000AFF] blur-[100px] absolute z-[1] bottom-[22%] right-[40%] animate-pulse'></div>
-            <div className='w-[206px] h-[206px] bg-[#0079FF] blur-[100px] absolute z-[1] bottom-[19%] right-[50%] animate-pulse'></div>
+            <div className='w-[206px] h-[206px] bg-[#8000FF] blur-[100px] absolute z-[1] bottom-[13%] sm:bottom-[20%] right-[30%] animate-pulse'></div>
+            <div className='w-[206px] h-[206px] bg-[#000AFF] blur-[100px] absolute z-[1] bottom-[13%] sm:bottom-[22%] right-[40%] animate-pulse'></div>
+            <div className='w-[206px] h-[206px] bg-[#0079FF] blur-[100px] absolute z-[1] bottom-[13%] sm:bottom-[19%] right-[50%] animate-pulse'></div>
             <div className='w-[167px] h-[167px] bg-[#00BCD4] absolute top-[39%] left-[-140px] blur-[200px]'></div>
             <div className='w-[167px] h-[167px] bg-[#00BCD4] absolute top-[-3%] right-[-140px] blur-[140px]'></div>
             <div className='py-4 border-t-[0.5px] border-solid border-[#ffffff66] mt-[16px] relative z-10'>
